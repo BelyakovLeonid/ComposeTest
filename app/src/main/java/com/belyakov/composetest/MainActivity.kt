f4717e558c73aaa8c9a15cda93811fe22417170e
Belyakov <lenyabelyakov@mail.ru>
2022-08-25 00:29:49 +0300

    Step 6. State, Remember

--- app/src/main/java/com/belyakov/composetest/MainActivity.kt
+++ app/src/main/java/com/belyakov/composetest/MainActivity.kt
@@ -3,130 +3,71 @@ package com.belyakov.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
-            ) {
-                Test()
-            }
+            Test()
         }
     }
 }
 
-val fontFamily = FontFamily(
-    Font(R.font.montserrat_thin, FontWeight.Thin),
-    Font(R.font.montserrat_light, FontWeight.Light),
-    Font(R.font.montserrat_regular, FontWeight.Normal),
-    Font(R.font.montserrat_medium, FontWeight.Medium),
-    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
-    Font(R.font.montserrat_bold, FontWeight.Bold),
-)
-
 @Composable
-fun ImageCard(
-    paint: Painter,
-    title: String,
-    contentDescription: String,
-    modifier: Modifier = Modifier
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    onClick: (Color) -> Unit
 ) {
-
-    Card(
-        modifier = modifier,
-        elevation = 8.dp,
-        shape = RoundedCornerShape(12.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = paint,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black),
-                            startY = 200F
-                        )
-                    )
-                    .padding(30.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 10.sp
-                            )
-                        ) {
-                            append(title)
-                        }
-                        append("\n\n")
-                        append(title)
-                    },
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 30.sp,
-                        fontFamily = fontFamily,
-                        fontStyle = FontStyle.Italic,
-                        textAlign = TextAlign.End,
-                        textDecoration = TextDecoration.LineThrough
+    Box(
+        modifier = modifier
+            .background(Color.Blue)
+            .clickable {
+                onClick(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1F
                     )
                 )
             }
-        }
-    }
+    )
 }
 
 @Preview
 @Composable
 fun Test() {
-    ImageCard(
-        paint = painterResource(id = R.drawable.img_test),
-        title = "This is a test title",
-        contentDescription = "This is a test desc",
-        modifier = Modifier.fillMaxSize(0.5F)
-    )
+    val state = remember {
+        mutableStateOf(Color.Yellow)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        ColorBox(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1F)
+        ) { newColor ->
+            state.value = newColor
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(state.value)
+                .weight(1F)
+        )
+    }
 }
