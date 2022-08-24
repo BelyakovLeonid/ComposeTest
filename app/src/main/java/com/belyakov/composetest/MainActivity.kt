8a3d7c8ec698236ea9f83cb00839553690803142
Belyakov <lenyabelyakov@mail.ru>
2022-08-24 12:14:47 +0300

    Step 4. Card and Images

--- app/src/main/java/com/belyakov/composetest/MainActivity.kt
+++ app/src/main/java/com/belyakov/composetest/MainActivity.kt
@@ -1,52 +1,96 @@
 package com.belyakov.composetest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(color = Color.Green)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Black)
-                    .padding(10.dp)
-                    .border(8.dp, Color.Black)
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(16.dp)
             ) {
-                Text(
-                    modifier = Modifier.clickable { Log.d("MyTag", "click") },
-                    text = "1Hello there!"
-                )
-                Spacer(
-                    modifier = Modifier
-                        .height(20.dp)
-                        .fillMaxWidth()
-                        .background(Color.Black)
-                )
-                Text("11Hello there!")
-                Text("111Hello there!")
+                Test()
             }
         }
     }
+}
+
+@Composable
+fun ImageCard(
+    paint: Painter,
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier,
+        elevation = 8.dp,
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = paint,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 200F
+                        )
+                    )
+                    .padding(30.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 30.sp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Test() {
+    ImageCard(
+        paint = painterResource(id = R.drawable.img_test),
+        title = "This is a test title",
+        contentDescription = "This is a test desc",
+        modifier = Modifier.fillMaxSize(0.5F)
+    )
 }
