0cd2e09d3e97826e5d3f896e21db30e89909bcbb
Belyakov <lenyabelyakov@mail.ru>
2022-08-25 18:19:09 +0300

    Step 8. ConstraintLayout

--- app/src/main/java/com/belyakov/composetest/MainActivity.kt
+++ app/src/main/java/com/belyakov/composetest/MainActivity.kt
@@ -3,35 +3,76 @@ package com.belyakov.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            LazyColumn {
-                itemsIndexed(listOf("one", "two", "three", "four")) { index, item ->
-                    Box(
-                        modifier = Modifier
-                            .clickable { }
-                            .fillMaxWidth()
-                            .height(100.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "$item #$index")
-                    }
-                }
-            }
+            Main()
         }
+    }
+
+    @Composable
+    fun Main() {
+        val constraintSet = ConstraintSet {
+            val firstBox = createRefFor("First")
+            val secondBox = createRefFor("Second")
+
+            constrain(firstBox) {
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(secondBox.top)
+            }
+            constrain(secondBox) {
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(firstBox.bottom)
+                bottom.linkTo(parent.bottom)
+            }
+
+            createVerticalChain(firstBox, secondBox, chainStyle = ChainStyle.Packed)
+        }
+
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize(),
+            constraintSet = constraintSet
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .background(Color.Yellow)
+                    .layoutId("First")
+            )
+            Box(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .background(Color.Green)
+                    .layoutId("Second")
+            )
+        }
+    }
+
+    @Composable
+    @Preview
+    fun Test() {
+        Main()
     }
 }
